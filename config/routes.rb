fe4a3d4f78b18ac74ffe52efb6b45c115bf2da0f
david lugo <davidlugo936@gmail.com>
2021-03-02 15:03:35 -0500

    implemented some css components for index page, finished backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
 
   resources :cocktails, only: [:show, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
-
+  resources :doses, only: [:destroy]
 end
- 
